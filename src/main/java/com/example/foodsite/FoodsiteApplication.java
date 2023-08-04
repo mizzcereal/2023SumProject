8f2ca2f8ce4c712689aacfbc6d2455c83190eed2
mizzcereal <sangwookpen96@gmail.com>
2023-08-04 10:04:01 +0900

    프로젝트시작

---
+++ src/main/java/com/example/foodsite/FoodsiteApplication.java
@@ -0,0 +1,13 @@
+package com.example.foodsite;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FoodsiteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FoodsiteApplication.class, args);
+	}
+
+}
